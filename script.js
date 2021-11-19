f264cce649729d3dd4d590024664415abe9cdd23
Eric-Gona <eric.gona254@gmail.com>
2021-11-19 10:03:27 +0300

    adds delani-studio files

---
+++ script.js
@@ -0,0 +1,16 @@
+
+    
+
+
+
+<script type="text/javascript">
+
+jQuery(window).load(function() {
+var container = document.querySelector('#container');
+var msnry = new Masonry( container, {
+itemSelector: '.masonryImage',
+columnWidth: '.masonryImage',                
+});  
+
+});
+</script>
